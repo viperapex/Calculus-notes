03ef18e789938f6d0339900cc588a9fb84e32eed
-Drunken_Viper - <viperapex@github.com>
2025-10-22 03:16:04 +0100

    added slope calculation

--- The_Delta_Method.py
+++ The_Delta_Method.py
@@ -17,8 +17,8 @@ y = f(x)
 fig, ax = plt.subplots()
 plt.axvline(x=0, color='lightgray')
 plt.axhline(y=0, color='lightgray')
-plt.scatter(2, 10)  # new
-plt.scatter(5, 37, c='orange', zorder=3)  # new
+plt.scatter(2, 10)  # Point P
+plt.scatter(5, 37, c='orange', zorder=3)  # Point Q
 _ = ax.plot(x, y)
 plt.show()
 
@@ -26,3 +26,73 @@ plt.show()
 # Indentifying what the value of y is at x
 z = f(2)
 print(z)
+
+
+# Finding the slope m between points P and Q
+m = (37-10)/(5-2)
+print(m)
+
+# To plot the line that passes through P and Q , We can rearrange the equation of a line y = mx+b:
+b = 37-m*5
+print(b)
+
+
+line_y = m*x+b
+
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+plt.scatter(2, 10)  # Point P
+plt.scatter(5, 37, c='orange', zorder=3)  # Point Q
+plt.ylim(-5, 150)
+plt.plot(x, line_y, c='orange')
+_ = ax.plot(x, y)
+plt.show()
+
+e = f(2.1)
+print(e)
+
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+plt.scatter(2, 10)  # Point P
+plt.scatter(2.1, 10.61, c='orange', zorder=3)  # Point Q
+_ = ax.plot(x, y)
+plt.show()
+
+m = (10.61-10)/(2.1-2)
+print(m)
+
+b = 10.61-m*2.1
+print(b)
+
+line_y = m*x + b
+
+
+# The closer Q becomes to P(i.e. delta x approaches 0) it becomes clearer that
+# the slope m at point P = (2, 10) is equal to 6
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+plt.scatter(2.1, 10)  # Point P
+plt.scatter(2.1, 10.61, c='orange', zorder=3)  # Point Q
+plt.ylim(-5, 150)
+plt.plot(x, line_y, c='orange', zorder=3)
+_ = ax.plot(x, y)
+plt.show()
+
+
+delta_x = 0.000001
+print(delta_x)
+
+x1 = 2
+y1 = 10
+
+x2 = x1 + delta_x
+print(x2)
+
+y2 = f(x2)
+print(y2)
+
+m = (y2 - y1)/(x2 - x1)
+print(m)
