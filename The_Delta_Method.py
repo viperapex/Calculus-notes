16c61c62acdfaece83ebf3fe7fe344b91c4e24e9
-Drunken_Viper - <viperapex@github.com>
2025-10-22 19:20:51 +0100

    added derivative calculation

--- The_Delta_Method.py
+++ The_Delta_Method.py
@@ -96,3 +96,52 @@ print(y2)
 
 m = (y2 - y1)/(x2 - x1)
 print(m)
+
+
+x1 = -1
+
+y1 = f(x1)
+print(y1)
+
+print(delta_x)
+
+
+x2 = x1 + delta_x
+print(x2)
+
+y2 = f(x2)
+print(y2)
+
+y2 = f(x1 + delta_x)
+print(y2)
+
+m = (y2-y1)/(x2-x1)
+print(m)
+
+b = y2-m*x2
+print(b)
+
+line_y = m*x + b
+
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+plt.scatter(x1, y1)  # Point P
+plt.scatter(x2, y2, c='orange', zorder=3)  # Point Q
+plt.ylim(-5, 150)
+plt.plot(x, line_y, c='orange', zorder=3)
+_ = ax.plot(x, y)
+plt.show()
+
+
+def diff_demo(my_f, my_x, my_delta):
+    return (my_f(my_x + my_delta) - my_f(my_x)) / my_delta
+
+
+deltas = [1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001]
+
+for delta in deltas:
+    print(diff_demo(f, 2, delta))
+
+for delta in deltas:
+    print(diff_demo(f, -1, delta))
