59cc958554fcb19f9fae6776dd6552e1dbfbd0ef
-Drunken_Viper - <viperapex@github.com>
2025-10-24 15:16:42 +0100

    added regression plot function

--- Automatic_Differentiation_in_Pytorch.py.py
+++ Automatic_Differentiation_in_Pytorch.py.py
@@ -1,13 +1,43 @@
 import torch
+import matplotlib.pyplot as plt
 
-x = torch.tensor(5.0)
-
+# E.g. :Dosage of drug for treating Alzheimer's disease
+x = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7])
 print(x)
 
-x.requires_grad_()  # Contagiously track gradients through forward pass
+# patients forgetfulness score
+y = torch.tensor([1.86, 1.31, 0.62, 0.33, 0.09, -0.67, -1.23, -1.37])
+print(y)
 
-y = x**2
+fig, ax = plt.subplots()
+plt.title("Clinical Trial")
+plt.xlabel("Drug dosage (ml)")
+plt.ylabel("Forgetfulness")
+_ = ax.scatter(x, y)
+# plt.show()
 
-y.backward()  # use autodiff
+m = torch.tensor([0.9]).requires_grad_()
+print(m)
+b = torch.tensor([0.1]).requires_grad_()
+print(b)
 
-print(x.grad)
+
+def regression_plot(my_x, my_y, my_m, my_b):
+    fig, ax = plt.subplots()
+    ax.scatter(my_x, my_y)
+
+    x_min, x_max = ax.get_xlim()
+
+    # Convert tensors to numpy arrays and detach from computation graph
+    my_m_np = my_m.detach().numpy()[0]  # Use [0] to get scalar value
+    my_b_np = my_b.detach().numpy()[0]  # Use [0] to get scalar value
+
+    y_min = my_m_np * x_min + my_b_np
+    y_max = my_m_np * x_max + my_b_np
+
+    ax.set_xlim([x_min, x_max])
+    _ = ax.plot([x_min, x_max], [y_min, y_max])
+    plt.show()
+
+
+regression_plot(x, y, m, b)
--- Line_Equation_as_a_Tensor_Graph.py
+++ Line_Equation_as_a_Tensor_Graph.py
@@ -18,4 +18,27 @@ plt.title("Clinical Trial")
 plt.xlabel("Drug dosage (ml)")
 plt.ylabel("Forgetfulness")
 _ = ax.scatter(x, y)
-plt.show()
+# plt.show()
+
+
+m = torch.tensor([0.9]).requires_grad_()
+print(m)
+b = torch.tensor([0.1]).requires_grad_()
+print(b)
+
+
+def regression_plot(my_x, my_y, my_m, my_b):
+
+    fig, ax = plt.subplots()
+
+    ax.scatter(my_x, my_y)
+
+    x_min, x_max = ax.get_xlim()
+    y_min, y_max = my_m*x_min + my_b, my_m*x_max + my_b
+
+    ax.set_xlim([x_min, x_max])
+    _ = ax.plot([x_min, x_max], [y_min, y_max])
+    plt.show()
+
+
+regression_plot(x, y, m, b)
