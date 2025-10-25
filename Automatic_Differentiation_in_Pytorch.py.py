8158dcbad4a135f4e3a86173e421261705c8981b
-Drunken_Viper - <viperapex@github.com>
2025-10-25 17:51:01 +0100

    added code for SGD optimization

--- Automatic_Differentiation_in_Pytorch.py.py
+++ Automatic_Differentiation_in_Pytorch.py.py
@@ -22,6 +22,10 @@ b = torch.tensor([0.1]).requires_grad_()
 print(b)
 
 
+def regression(my_x, my_m, my_b):
+    return my_m*my_x + my_b
+
+
 def regression_plot(my_x, my_y, my_m, my_b):
     fig, ax = plt.subplots()
     ax.scatter(my_x, my_y)
@@ -41,3 +45,50 @@ def regression_plot(my_x, my_y, my_m, my_b):
 
 
 regression_plot(x, y, m, b)
+
+"""
+ It doesn't yet include:
+
+Loss calculation
+
+Gradient descent optimization
+
+Training loop to update m and b
+"""
+
+# Forward Pass
+
+yhat = regression(x, m, b)  # y^ refs newtons notation
+print(yhat)
+
+
+def mse(my_yhat, my_y):
+    sigma = torch.sum((my_yhat - my_y)**2)
+    return sigma/len(my_y)
+
+
+C = mse(yhat, y)
+
+print(C)
+
+
+# PyTorch Autodiff to calculate gradient of C(Cosst) wrt parameters m and b
+C.backward()  # differentiating backwards
+
+print(m.grad)
+
+print(b.grad)
+
+# We pass in the parameters along with learning rate 'lr' to Stochastic gradient descent optimizer function
+optimizer = torch.optim.SGD([m, b], lr=0.01)
+optimizer.step()
+
+# we see that the parameters are optimized and the cost is lowered
+print(m)
+print(b)
+
+
+regression_plot(x, y, m, b)
+
+C = mse(regression(x, m, b), y)
+print(C)
