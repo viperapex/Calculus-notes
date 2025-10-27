ecb6f2a23707dd60a7a02e73240b97bb8d9bc92f
-Drunken_Viper - <viperapex@github.com>
2025-10-27 17:50:52 +0000

    defined functions and variables, added plot

--- Partial_Derivatives_of_Multivariate_Functions.py
+++ Partial_Derivatives_of_Multivariate_Functions.py
@@ -2,3 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import math  # for constant pi
+
+
+def f(my_x, my_y):
+    return my_x**2 - my_y**2
+
+
+# plotting z wrt to x by varying x
+xs = np.linspace(-3, 3, 1000)
+
+# while holding y constant(e.g. y = 0)
+zs_wrt_x = f(xs, 0)
+
+# plotting
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+
+plt.xlabel('x')
+plt.ylabel('z')
+_ = ax.plot(xs, zs_wrt_x)
+plt.show()
