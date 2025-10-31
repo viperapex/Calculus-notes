678cf89bd49bdd81e7c68e6beb90d405e3af13a4
-Drunken_Viper - <viperapex@github.com>
2025-10-31 04:18:58 +0000

    Partial derivative exercise 2 solution

--- PD_ex.py
+++ PD_ex.py
@@ -30,4 +30,25 @@ print(y.grad)
 
 # Task2-> Find all the  partial derivatives of the surface area of a cylinder i.e. 2pir**2 +2pi*r*h
 
+# define tensor graph for equation
+def sa_of_cylinder(my_r, my_h):
+    return 2*math.pi*my_r**2 + 2*math.pi*my_r*my_h
+
+# lets take arbitary values for each variable
+
+
+r = torch.tensor(2.).requires_grad_()
+print(r)
+
+h = torch.tensor(4.).requires_grad_()
+print(h)
+
+z = sa_of_cylinder(r, h)
+print(z)
+
+z.backward()
+print(r.grad)
+
+print(h.grad)
+
 # Task3-> Find all the  partial derivatives of the square prism with a cube cut out of it's centre is described by v=x**2*y-z**3
