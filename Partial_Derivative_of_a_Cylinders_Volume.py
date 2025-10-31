01f6416a7588089dcb1b958c01f026e1b2580f8a
-Drunken_Viper - <viperapex@github.com>
2025-10-31 03:48:38 +0000

    added proofs

--- Partial_Derivative_of_a_Cylinders_Volume.py
+++ Partial_Derivative_of_a_Cylinders_Volume.py
@@ -13,7 +13,7 @@ r = torch.tensor(3.).requires_grad_()
 print(r)
 
 # ..and length is 5 meters.
-l = torch.tensor(5., requires_grad=True)
+l = torch.tensor(5.).requires_grad_()
 print(l)
 
 # Then the volume of the cylinder is 121.4 cubic meters:
@@ -21,16 +21,34 @@ v = cylinder_vol(r, l)
 print(v)
 
 v.backward()  # del v
-
-# /del l ---shows the gradient val or partial derivative when l is kept constant.
+# /del l ---shows the gradient val or partial derivative of wrt  l when l is kept constant.
 print(l.grad)
+
 
 print(math.pi * 3**2)  # same value when done manually
 # This means that with r=3, a change in l by one unit corresponds to a change in v of 28.27m**3.
 
-# lets prove this by different inputs
+# lets prove this by different input instead of l=5 we take l=6
 
-a = cylinder_vol(3, 6)
-print(a)
+# difference bw a and v is 28.3 which proves the above
+cylinder_vol(3, 6) - cylinder_vol(3, 5)
 
-print(a-v)
+
+# v.backward # del v
+# /del r ---shows the gradient val or partial derivative for r when 2pi and l are kept constant.
+print(r.grad)
+
+
+print(2 * math.pi * 3 * 5)  # same value when done manually
+
+"""
+r is includeed in the partial derivative do adjusting it affects the scale
+of its impacton v. ALthough it's our first example in this notebook, 
+it is typical in calculus for the derivative only to apply at an infinitesimally small 'delta r'.
+The smaller the 'delta r', the closer to the true 'pdel v/ pdel r.
+"""
+
+delta = 1e-6
+
+# Dividing by delta restores scale
+print((cylinder_vol(3 + delta, 5) - cylinder_vol(3, 5)) / delta)
